aba0272fde88dabd3a6b8b664d1c451b4f49dc2a
John Leider <john@vuetifyjs.com>
2025-06-02 10:17:29 -0500

    refactor: add logger, move cloner to task

--- src/tasks/generate-api.ts
+++ src/tasks/generate-api.ts
@@ -1,15 +1,21 @@
 import { exec } from 'child_process'
 import { promisify } from 'util'
 import { rimraf } from 'rimraf'
+import { logger } from '#utils/logger'
 
 const execAsync = promisify(exec)
 
 async function main () {
   await rimraf('api')
+  await rimraf('temp')
+
+  logger.info('Installing Vuetify dependencies and building API...')
   await execAsync('git clone --depth=1 git@github.com:vuetifyjs/vuetify.git temp/vuetify')
-  await execAsync('cd temp/vuetify && pnpm patch-remove @testing-library/vue vue-gtag-next && pnpm install --frozen-lockfile && pnpm build vuetify && pnpm build api')
+  await execAsync('cd temp/vuetify && pnpm install --frozen-lockfile && pnpm build vuetify && pnpm build api')
   await execAsync('cp -r temp/vuetify/packages/api-generator/dist/api api/')
   await rimraf('temp')
+
+  logger.info('Vuetify API generated successfully.')
 }
 
 await main()
---
+++ src/utils/logger.ts
@@ -0,0 +1,13 @@
+import { pino } from 'pino'
+
+export const logger = pino({
+  level: 'info',
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      translateTime: 'SYS:standard',
+      ignore: 'pid,hostname'
+    }
+  }
+})
